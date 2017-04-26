c138d97121f70f89d9af2db2fff10605e8fd73db
lucaslEliane <517197934@qq.com>
2017-04-26 14:45:55 +0800

    修改单元测试用例

--- client/common/.internal/_test_/dateConvert.spec.js
+++ client/common/.internal/_test_/dateConvert.spec.js
@@ -13,6 +13,9 @@ describe('date convert function test', () => {
   it('use the default parameter', () => {
     expect(dateConvert()).toEqual('0秒前')
   })
+  /**
+   * 单元测试用例结果和当前时间有关，所以需要测试的时候请修改单元测试用例时间
+   */
   it('legal string', () => {
     expect(dateConvert('2017年4月1日 11:11:11')).toEqual('25天前')
     expect(dateConvert('11:11:11')).toEqual('1分钟前')
